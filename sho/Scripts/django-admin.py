1e2557dc42a1f17353f79862fe43a43eb4cad536
inth8204 <inth8204@gmail.com>
2020-01-15 20:27:16 +0900

    create shopping mall

---
+++ sho/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\shopping_mall\django-shop\sho\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
